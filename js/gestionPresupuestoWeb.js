50a2b6ca414e4905364d5d11d25add68ac9462bb
Javier <javninpen@alu.edu.gva.es>
2026-02-02 18:49:55 +0100

    Añadir gráficos de gastos agrupados utilizando Chart.js en la función mostrarGastosAgrupadosWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -112,6 +112,8 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
         console.log(`El elemento ${idElemento} no existe o no se encuentra en este documento`)
     }
     else {
+        var divP = document.getElementById(idElemento);
+        divP.innerHTML = "";
         const divAgrupacion = document.createElement("div")
         divAgrupacion.className = "agrupacion"
 
@@ -138,6 +140,65 @@ function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo) {
             divDato.appendChild(spanValor)
             divAgrupacion.appendChild(divDato)
         }
+        // Estilos
+        divP.style.width = "33%";
+        divP.style.display = "inline-block";
+        // Crear elemento <canvas> necesario para crear la gráfica
+        // https://www.chartjs.org/docs/latest/getting-started/
+        let chart = document.createElement("canvas");
+        // Variable para indicar a la gráfica el período temporal del eje X
+        // En función de la variable "periodo" se creará la variable "unit" (anyo -> year; mes -> month; dia -> day)
+        let unit = "";
+        switch (periodo) {
+            case "anyo":
+                unit = "year";
+                break;
+            case "mes":
+                unit = "month";
+                break;
+            case "dia":
+            default:
+                unit = "day";
+                break;
+        }
+
+        // Creación de la gráfica
+        // La función "Chart" está disponible porque hemos incluido las etiquetas <script> correspondientes en el fichero HTML
+        const myChart = new Chart(chart.getContext("2d"), {
+            // Tipo de gráfica: barras. Puedes cambiar el tipo si quieres hacer pruebas: https://www.chartjs.org/docs/latest/charts/line.html
+            type: 'bar',
+            data: {
+                datasets: [
+                    {
+                        // Título de la gráfica
+                        label: `Gastos por ${periodo}`,
+                        // Color de fondo
+                        backgroundColor: "#555555",
+                        // Datos de la gráfica
+                        // "agrup" contiene los datos a representar. Es uno de los parámetros de la función "mostrarGastosAgrupadosWeb".
+                        data: agrup
+                    }
+                ],
+            },
+            options: {
+                scales: {
+                    x: {
+                        // El eje X es de tipo temporal
+                        type: 'time',
+                        time: {
+                            // Indicamos la unidad correspondiente en función de si utilizamos días, meses o años
+                            unit: unit
+                        }
+                    },
+                    y: {
+                        // Para que el eje Y empieza en 0
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+        // Añadimos la gráfica a la capa
+        divP.append(chart);
     }
 }
 function repintar() {
@@ -148,6 +209,7 @@ function repintar() {
     mostrarDatoEnId("balance-total", presupuesto.calcularBalance())
 
     mostrarGastoWeb("listado-gastos-completo", presupuesto.listarGastos())
+
 }
 function actualizarPresupuestoWeb() {
     let nuevoPresupuesto = prompt("Ingrese el nuevo valor del presupuesto")
@@ -412,13 +474,13 @@ function EnviarApiHandleFormulario() {
         valor = Number(valor)
         let fecha = form.querySelector("#fecha").value
         let etiqueta = form.querySelector("#etiquetas").value
-        if (etiqueta === ""){
+        if (etiqueta === "") {
             etiqueta = []
         }
-        else{
+        else {
             etiqueta = etiqueta.split(",")
         }
-        
+
 
         let nuevoGasto = new presupuesto.CrearGasto(descripcion, valor, fecha, ...etiqueta)
         let nuevoGastoJSON = JSON.stringify(nuevoGasto)
